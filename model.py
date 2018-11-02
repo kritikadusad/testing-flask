8ff85c7f8558db897341ffe2c67be35238b4686f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-11-02 12:33:46 -0700

    Completed

---
+++ model.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Board game."""
+
+    __tablename__ = "games"
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(100))
+
+
+def connect_to_db(app, db_uri="postgresql:///games"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
+
+
+def example_data():
+    """Create example data for the test database."""
+    # FIXME: write a function that creates a game and adds it to the database.
+    Game.query.delete()
+
+    # Add sample games
+    game1 = Game(name='Tetris', description='make falling blocks fit perfectly')
+    game2 = Game(name='Beer pong', description='Drink beer while throwing balls into cups')
+
+    db.session.add_all([game1, game2])
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    from party import app
+
+    connect_to_db(app)
+    print("Connected to DB.")
